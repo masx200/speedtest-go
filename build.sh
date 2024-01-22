7a8abb1639f3ba457ab48bf3036d21825956d09f
masx200 <34191203+masx200@users.noreply.github.com>
2024-01-22 20:14:58 +0800

    linux-mipsle-softfloat

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ mkdir temp
 mkdir dist
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0  go build -v -ldflags="-w -s" -o ./dist/linux-amd64/speedtest main.go
 GOOS=windows GOARCH=amd64 CGO_ENABLED=0  go build -v -ldflags="-w -s" -o ./dist/windows-amd64/speedtest.exe main.go 
-CC=mips-linux-gcc GOOS=linux GOARCH=mipsle CGO_ENABLED=0 GOMIPS=softfloat go build -v -ldflags="-w -s" -o ./dist/linux-mipsle/speedtest main.go 
+CC=mips-linux-gcc GOOS=linux GOARCH=mipsle CGO_ENABLED=0 GOMIPS=softfloat go build -v -ldflags="-w -s" -o ./dist/linux-mipsle-softfloat/speedtest main.go 
 GOOS=linux GOARCH=arm64 CGO_ENABLED=0  go build -v -ldflags="-w -s" -o ./dist/linux-arm64/speedtest main.go  
 wget --output-document=./temp/speedtest-go-main.zip https://github.com/masx200/speedtest-go/archive/refs/heads/main.zip
 deno run -A ./build.ts
--- build.ts
+++ build.ts
@@ -4,79 +4,79 @@ import { JSZipGeneratorOptions } from "https://deno.land/x/jszip@0.11.0/types.ts
 import { resolve } from "https://deno.land/std@0.181.0/path/mod.ts";
 import { type JSZipFileOptions } from "https://deno.land/x/jszip@0.11.0/types.ts";
 if (import.meta.main) {
-    const source = resolve("./temp/speedtest-go-main.zip");
-    const fileoptions = { unixPermissions: "777" } satisfies JSZipFileOptions;
-    const generateoptions = {
-        compression: "DEFLATE",
-    } satisfies JSZipGeneratorOptions<"uint8array">;
-    await Promise.all([
-        addFileToZipFileOutput(
-            source,
-            "speedtest-go-main/speedtest.exe",
-            "./dist/windows-amd64/speedtest.exe",
-            "./dist/windows-amd64-speedtest.zip",
-            fileoptions,
-            generateoptions
-        ),
-        addFileToZipFileOutput(
-            source,
-            "speedtest-go-main/speedtest",
-            "./dist/linux-amd64/speedtest",
-            "./dist/linux-amd64-speedtest.zip",
-            fileoptions,
-            generateoptions
-        ),
-        addFileToZipFileOutput(
-            source,
-            "speedtest-go-main/speedtest",
-            "./dist/linux-arm64/speedtest",
-            "./dist/linux-arm64-speedtest.zip",
-            fileoptions,
-            generateoptions
-        ),
-        addFileToZipFileOutput(
-            source,
-            "speedtest-go-main/speedtest",
-            "./dist/linux-mipsle/speedtest",
-            "./dist/linux-mipsle-speedtest.zip",
-            fileoptions,
-            generateoptions
-        ),
-    ]);
+  const source = resolve("./temp/speedtest-go-main.zip");
+  const fileoptions = { unixPermissions: "777" } satisfies JSZipFileOptions;
+  const generateoptions = {
+    compression: "DEFLATE",
+  } satisfies JSZipGeneratorOptions<"uint8array">;
+  await Promise.all([
+    addFileToZipFileOutput(
+      source,
+      "speedtest-go-main/speedtest.exe",
+      "./dist/windows-amd64/speedtest.exe",
+      "./dist/windows-amd64-speedtest.zip",
+      fileoptions,
+      generateoptions
+    ),
+    addFileToZipFileOutput(
+      source,
+      "speedtest-go-main/speedtest",
+      "./dist/linux-amd64/speedtest",
+      "./dist/linux-amd64-speedtest.zip",
+      fileoptions,
+      generateoptions
+    ),
+    addFileToZipFileOutput(
+      source,
+      "speedtest-go-main/speedtest",
+      "./dist/linux-arm64/speedtest",
+      "./dist/linux-arm64-speedtest.zip",
+      fileoptions,
+      generateoptions
+    ),
+    addFileToZipFileOutput(
+      source,
+      "speedtest-go-main/speedtest",
+      "./dist/linux-mipsle-softfloat/speedtest",
+      "./dist/linux-mipsle-speedtest.zip",
+      fileoptions,
+      generateoptions
+    ),
+  ]);
 }
 export async function addFileToZipFileOutput(
-    source: string,
-    path: string,
-    file: string,
-    dest: string,
-    fileoptions: JSZipFileOptions = {},
-    generateoptions: JSZipGeneratorOptions<"uint8array"> = {}
+  source: string,
+  path: string,
+  file: string,
+  dest: string,
+  fileoptions: JSZipFileOptions = {},
+  generateoptions: JSZipGeneratorOptions<"uint8array"> = {}
 ) {
-    generateoptions = {
-        compression: "DEFLATE",
-        ...generateoptions,
-    };
-    console.log({
-        source: resolve(source),
-        path,
-        file: resolve(file),
-        dest: resolve(dest),
-        fileoptions,
-        generateoptions,
-    });
-    const zip = await readZip(resolve(source));
-    zip.addFile(path, await Deno.readFile(resolve(file)), fileoptions);
-    const outputfile = resolve(dest);
-    await writeZip(zip, outputfile, generateoptions);
+  generateoptions = {
+    compression: "DEFLATE",
+    ...generateoptions,
+  };
+  console.log({
+    source: resolve(source),
+    path,
+    file: resolve(file),
+    dest: resolve(dest),
+    fileoptions,
+    generateoptions,
+  });
+  const zip = await readZip(resolve(source));
+  zip.addFile(path, await Deno.readFile(resolve(file)), fileoptions);
+  const outputfile = resolve(dest);
+  await writeZip(zip, outputfile, generateoptions);
 }
 export async function writeZip(
-    zip: JSZip,
-    path: string,
-    options: JSZipGeneratorOptions<"uint8array"> = {}
+  zip: JSZip,
+  path: string,
+  options: JSZipGeneratorOptions<"uint8array"> = {}
 ) {
-    const b: Uint8Array = await zip.generateAsync({
-        type: "uint8array",
-        ...options,
-    });
-    return await Deno.writeFile(path, b);
+  const b: Uint8Array = await zip.generateAsync({
+    type: "uint8array",
+    ...options,
+  });
+  return await Deno.writeFile(path, b);
 }
